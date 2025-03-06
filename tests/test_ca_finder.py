d991f798950af8ec94a2958108ed778e568c6e85
GrossBetruger <koriatoren@gmail.com>
2025-03-06 12:14:08 +0200

    test: ca-finder test birdeye

--- tests/test_ca_finder.py
+++ tests/test_ca_finder.py
@@ -4,6 +4,7 @@ import pandas as pd
 from ca_finder.ca_finder import Chain, Confidence, find_relevant_adderess
 from ca_finder.ca_finder import find_relevant_adderess_outside_user_context
 from ca_finder.ca_finder import find_relevant_adderess_outside_content
+from ca_finder.ca_finder import query_birdeye_api_ticker
 
 
 def test_ca_finder():
@@ -61,3 +62,32 @@ def test_ca_finder():
     for addr in addresses:
         assert any(addr in tweet for tweet in match_df["Content"].to_list())
     print(f"Match DataFrame:\n{match_df}")
+
+
+def test_bird_api():
+    ca_address_1 = "0xBADFf0eF41D2A68F22De21EAbca8A59Aaf495cF0"
+    result1 = query_birdeye_api_ticker(ca_address_1, Chain.ETHEREUM)
+    assert result1 is not None
+    assert result1["success"] is True
+    assert "data" in result1
+    assert "address" in result1["data"]
+    assert result1["data"]["address"] == ca_address_1
+    assert result1["data"]["symbol"] == "KABOSU"
+
+    ca_address_2 = "0xDe5e66482A436fd437dE2cdEAc39aA212F0F10C1"
+    result2 = query_birdeye_api_ticker(ca_address_2, Chain.ETHEREUM)
+    assert result2 is not None
+    assert result2["success"] is True
+    assert "data" in result2
+    assert "address" in result2["data"]
+    assert result2["data"]["address"] == ca_address_2
+    assert result2["data"]["symbol"] == "FLOKI"
+
+    ca_address_3 = "7BPAjesYS2FCY84gzBmMbukFQqC6rLgtYP9twMvji6Pr"
+    result3 = query_birdeye_api_ticker(ca_address_3, Chain.SOLANA)
+    assert result3 is not None
+    assert result3["success"] is True
+    assert "data" in result3
+    assert "address" in result3["data"]
+    assert result3["data"]["address"] == ca_address_3
+    assert result3["data"]["symbol"] == "SHIB"
